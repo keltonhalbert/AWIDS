ef2a32eb6952fc99302a68ba27f1dd4b4ef86516
Kelton Halbert <keltonhalbert@tempestchasing.com>
2012-07-01 16:03:25 -0500

    added MESONET projection.

--- awids/Projection.py
+++ awids/Projection.py
@@ -152,6 +152,9 @@ class Projection( object ):
                   rsphere=(6378137.00,6356752.3142),\
                   resolution='l',area_thresh=1000.,projection='lcc',\
                   lat_1=40,lat_2=30,lat_0=40,lon_0=-76)
+    if area == 'MESONET':
+        map = Basemap(projection='lcc', resolution='l', width=grid_width, height=grid_height,
+                        lat_0=center_latitude, lon_0=center_longitude, lat_1=30., lat_2=45.)
     if area in StationID:
       lon_lat = self.StationDict[ area ]
       map = Basemap(width=1500000,height=1100000,
